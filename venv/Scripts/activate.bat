0cf6c269ad31f1024b03063fbde3b9fe0bfa467c
Arittra-Bag <110278170+Arittra-Bag@users.noreply.github.com>
2023-12-10 13:27:57 +0530

    Update-4

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\aritt\OneDrive\Desktop\AuzInfoTech\venv
+set VIRTUAL_ENV=C:\Users\aritt\OneDrive\Desktop\AuzInfoTech - Copy\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
